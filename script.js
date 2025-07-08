284c048eb9a4b77c85eb8abc52c18ff0b054f335
Raqilk <raqilehmedovs@gmail.com>
2025-07-08 15:04:50 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  document.querySelectorAll(".btn").forEach(btn=>{
-    btn.addEventListener("click", ()=>{
-      const action = btn.dataset-action;
-      if(action==="login")    window.location.href="login.html";
-      if(action==="register") window.location.href="register.html";
-    });
-  });
-});
